255338483e0b1f05dff6945c496ad285bed445dd
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-05-15 18:08:13 -0600

    finish form state functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import Envelope from "./components/Envelopes";
 import Form from "./components/Form";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState("");
+  const [envelopes, setEnvelopes] = useState("");
+  const [valueEnv, setValueEnv] = useState(0);
+
+  const handleSubmit = () => {
+    let value = amount / envelopes;
+    setValueEnv(value);
+  };
 
   return (
     <div className="app">
-      <Form />
+      <Form
+        amount={amount}
+        setAmount={setAmount}
+        envelopes={envelopes}
+        setEnvelopes={setEnvelopes}
+        handleSubmit={handleSubmit}
+      />
       <Envelope />
+      {/* {valueEnv} */}
     </div>
   );
 }
